df475f1b95e100e51fe3d6735a4a94bacfad32be
Samantha Catanjal <scatanjal@gmail.com>
2023-05-03 08:05:45 +0400

    added comments

--- Chapter-6-Javascript/Exercises/Petrol Calculator/script.js
+++ Chapter-6-Javascript/Exercises/Petrol Calculator/script.js
@@ -1,20 +1,19 @@
-document.getElementById('liter').value = 0;
-document.getElementById('cost').value = 1.75;
+document.getElementById('liter').value = 0; /* makes specified id input have a default value */
+document.getElementById('cost').value = 1.75; /* makes specified id input have a default value */
 
-const enter = document.getElementById('enter');
-const output = document.getElementById('result');
+const enter = document.getElementById('enter'); /* assigns makeshift enter button to a value */
+const output = document.getElementById('result'); /* assigns blank paragraph to a value */
 
-function enter_() {
-    if (document.getElementById('cost').value = 1.75) {
-        if (document.getElementById('liter').value > 0) {
-            const liter = document.getElementById('liter')
-            cost = 1.75
+function total() { /* creates instruction for calculation */
+    if (document.getElementById('liter').value > 0) { /* if liter is = 0, then...*/
+        const cost = document.getElementById('cost').value /* any input from user will be assigned as a value here */
+        const liter = document.getElementById('liter').value  /* any input from user will be assigned as a value here */
 
-            output.innerHTML = cost * liter.value + ' AED'
-        } else {
-            output.innerHTML = 0
-        }
+        output.innerHTML = cost * liter + ' AED' /* blank paragraph shows calculated result */
+
+    } else { /* if liter is <= 0, then... */
+        output.innerHTML = 0 /* blank paragraph shows 0 */
     }
 }
 
-enter.addEventListener('click', enter_)
+enter.addEventListener('click', total) /* when clicking enter button, it runs the function total()*/
